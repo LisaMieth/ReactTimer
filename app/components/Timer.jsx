2bdf3805bdbbeaec59cc19493fe08aec362fdd61
LisaMieth <lisahafizovic@gmail.com>
2017-01-29 20:43:51 +1100

    Add navigation

---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Timer component</p>
+      </div>
+    );
+  }
+});
+module.exports = Timer;
